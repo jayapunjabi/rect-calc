5325d4048576cadf9dc758030e129360103a650e
jayapunjabi <jayapunjabi707@gmail.com>
2020-08-16 15:10:43 +0530

    Updates

---
+++ precache-manifest.da9b911aa2631c3790fcc83082cc1a8b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cc59bb185e5688b11f22da8393f344c",
+    "url": "/react-calc/index.html"
+  },
+  {
+    "revision": "6e4b74b969500efb0f8c",
+    "url": "/react-calc/static/css/main.0c1f15cc.chunk.css"
+  },
+  {
+    "revision": "48926d9c80cce50b0776",
+    "url": "/react-calc/static/js/2.2135fec4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-calc/static/js/2.2135fec4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e4b74b969500efb0f8c",
+    "url": "/react-calc/static/js/main.3e940e04.chunk.js"
+  },
+  {
+    "revision": "c662eab9bcaa591f4d86",
+    "url": "/react-calc/static/js/runtime-main.d795a5a2.js"
+  }
+]);
